270077a6b2917e8de495454c0e0637d34ab9a490
Nadia Lefebvre <95084122+nadialefebvre@users.noreply.github.com>
2022-08-29 10:35:14 +0200

    feat: add urls for API

---
+++ src/utils/urls.ts
@@ -0,0 +1,8 @@
+// could not use the URL provided: CORS issue
+// const BASE_URL = ""
+// export const API_URL = (slug: string) => `${BASE_URL}/${slug}`
+// in App.tsx, would have been: fetch(API_URL("transactions")) and fetch(API_URL("categories"))
+
+export const CATEGORIES_API_URL = "https://api.jsonbin.io/v3/b/62a3239005f31f68b3bbc3d5"
+
+export const TRANSACTIONS_API_URL = "https://api.jsonbin.io/v3/b/62a323ee05f31f68b3bbc412"
