54e26fe13b1d45003200ccac7dcd142dcf13a40f
Nadia Lefebvre <95084122+nadialefebvre@users.noreply.github.com>
2022-08-29 11:03:16 +0200

    feat: add skeleton material ui component to loader

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,7 +1,20 @@
 import React from "react"
+import Skeleton from "@mui/material/Skeleton"
+import Stack from '@mui/material/Stack';
 
-const Loader = () => {
-  return <>Loading</>
+const Loader: React.FC = () => {
+  return (
+      <Stack spacing={4}>
+      <Skeleton variant="rectangular" height={80} animation="wave" />
+      <Skeleton variant="rectangular" height={80} animation="wave" />
+      <Skeleton variant="rectangular" height={80} animation="wave" />
+      <Skeleton variant="rectangular" height={80} animation="wave" />
+      <Skeleton variant="rectangular" height={80} animation="wave" />
+      <Skeleton variant="rectangular" height={80} animation="wave" />
+      <Skeleton variant="rectangular" height={80} animation="wave" />
+      <Skeleton variant="rectangular" height={80} animation="wave" />
+    </Stack>
+  )
 }
 
 export default Loader
