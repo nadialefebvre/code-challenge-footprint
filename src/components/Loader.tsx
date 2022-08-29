4a1b2da0d072844a35ad40201317223641c1e5b5
Nadia Lefebvre <95084122+nadialefebvre@users.noreply.github.com>
2022-08-29 11:34:21 +0200

    feat: remove stack component

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,10 +1,9 @@
 import React from "react"
 import Skeleton from "@mui/material/Skeleton"
-import Stack from '@mui/material/Stack';
 
 const Loader: React.FC = () => {
   return (
-      <Stack spacing={4}>
+    <>
       <Skeleton variant="rectangular" height={80} animation="wave" />
       <Skeleton variant="rectangular" height={80} animation="wave" />
       <Skeleton variant="rectangular" height={80} animation="wave" />
@@ -13,7 +12,7 @@ const Loader: React.FC = () => {
       <Skeleton variant="rectangular" height={80} animation="wave" />
       <Skeleton variant="rectangular" height={80} animation="wave" />
       <Skeleton variant="rectangular" height={80} animation="wave" />
-    </Stack>
+    </>
   )
 }
 
