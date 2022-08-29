1105ca925e7946769df27622ed6e42dd1a81c39a
Nadia Lefebvre <95084122+nadialefebvre@users.noreply.github.com>
2022-08-29 11:05:38 +0200

    feat: add sorting functions

--- src/pages/MyFootprint.tsx
+++ src/pages/MyFootprint.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useCallback, useEffect, useState } from "react"
 import { useNavigate } from "react-router-dom"
 import Container from "@mui/material/Container"
 import Box from "@mui/material/Box"
@@ -14,7 +14,8 @@ import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos"
 
 import Loader from "../components/Loader"
 
-import { Props } from "../types/types"
+import { CategoriesJSON, Props } from "../types/types"
+import { averageFootprintCalculation } from "../utils/calc"
 
 const MyFootprint: React.FC<Props> = ({
   categories,
@@ -22,6 +23,45 @@ const MyFootprint: React.FC<Props> = ({
   isLoading,
 }: Props) => {
   const navigate = useNavigate()
+
+  const [sortedCategories, setSortedCategories] = useState<CategoriesJSON>([])
+
+  const sortByMainCategoryAZ = useCallback(() => {
+    const sortedAZ = [...categories].sort((a, b) => {
+      const mainCategoryA = a.mainCategory
+      const mainCategoryB = b.mainCategory
+
+      if (mainCategoryA < mainCategoryB) {
+        return -1
+      } else if (mainCategoryA > mainCategoryB) {
+        return 1
+      } else {
+        return 0
+      }
+    })
+    setSortedCategories(sortedAZ)
+  }, [categories])
+
+  const sortByImpactInDescendingOrder = () => {
+    const sortedImpact = [...categories].sort((a, b) => {
+      const footprintA = averageFootprintCalculation(
+        a.mainCategory,
+        transactions
+      )
+      const footprintB = averageFootprintCalculation(
+        b.mainCategory,
+        transactions
+      )
+      return footprintB - footprintA
+    })
+    setSortedCategories(sortedImpact)
+  }
+
+  useEffect(() => {
+    if (!isLoading) {
+      sortByMainCategoryAZ()
+    }
+  }, [isLoading, sortByMainCategoryAZ])
 
   return (
     <Container maxWidth="md">
@@ -34,45 +74,55 @@ const MyFootprint: React.FC<Props> = ({
           color="inherit"
           variant="text"
           aria-label="text button group"
-          style={{
-            marginBottom: "10px",
-          }}
+          style={{ marginBottom: "10px" }}
         >
-          <Button color="inherit" onClick={() => console.log("A..Z clicked")}>
+          <Button color="inherit" onClick={sortByMainCategoryAZ}>
             A..Z
           </Button>
-          <Button color="inherit" onClick={() => console.log("Impact clicked")}>
+          <Button color="inherit" onClick={sortByImpactInDescendingOrder}>
             Impact
           </Button>
         </ButtonGroup>
       </Box>
       <Stack spacing={4}>
-        {isLoading ? <Loader /> : categories.map(category => (
-          <Card key={category.mainCategoryID}>
-                <CardContent>
-                  <Stack direction="row" justifyContent="space-between" alignItems="center">
-                    <Typography variant="h5" color="text.secondary">
-                      {category.mainCategory}
-                    </Typography>
-                    <Stack direction="row" justifyContent="space-between" alignItems="center">
+        {isLoading ? (
+          <Loader />
+        ) : (
+          sortedCategories.map(category => (
+            <Card key={category.mainCategoryID}>
+              <CardContent>
+                <Stack
+                  direction="row"
+                  justifyContent="space-between"
+                  alignItems="center"
+                >
+                  <Typography variant="h5" color="text.secondary">
+                    {category.mainCategory}
+                  </Typography>
+                  <Stack
+                    direction="row"
+                    justifyContent="space-between"
+                    alignItems="center"
+                  >
                     <Chip
                       label="averageFootprint"
                       color="warning"
                       variant="outlined"
                       sx={{ width: "150px" }}
                     />
-                      <IconButton
-                        onClick={() =>
-                          navigate(`/transactions/${category.mainCategory}`)
-                        }
-                      >
-                        <ArrowForwardIosIcon />
-                      </IconButton>
-                    </Stack>
+                    <IconButton
+                      onClick={() =>
+                        navigate(`/transactions/${category.mainCategory}`)
+                      }
+                    >
+                      <ArrowForwardIosIcon />
+                    </IconButton>
                   </Stack>
-                </CardContent>
-          </Card>
-        ))}
+                </Stack>
+              </CardContent>
+            </Card>
+          ))
+        )}
       </Stack>
     </Container>
   )
